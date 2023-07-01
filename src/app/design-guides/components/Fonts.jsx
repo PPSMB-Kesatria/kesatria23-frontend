203f927700e546fd27a3b7b405d387f9416666f4
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-01 15:39:13 +0700

    feat(DesignGuides): add Fonts component

---
+++ src/app/design-guides/components/Fonts.jsx
@@ -0,0 +1,15 @@
+export default function Fonts() {
+  return (
+    <section className="font-creato flex flex-col justify-center items-center text-[30px] ">
+      <h1 className="font-alstoria text-[40px]">Fonts</h1>
+      <div className="flex gap-10">
+        <p className="font-alstoria">Alstoria</p>
+        <p>.font-alstoria</p>
+      </div>
+      <div className="flex gap-10">
+        <p className="font-creato">Creato Display</p>
+        <p>.font-creato</p>
+      </div>
+    </section>
+  );
+}
