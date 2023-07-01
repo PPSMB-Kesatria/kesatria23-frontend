e6fe3a26aa7c89720b006de8377c07e2e9419e28
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-01 15:37:11 +0700

    feat: import color palletes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        green: {
+          100: "#6DD5B5",
+          200: "#54BB9C",
+          300: "#439179",
+          400: "#004B47",
+        },
+        red: {
+          100: "#F17471",
+          200: "#F34B4C",
+          300: "#A51D1E",
+        },
+        neutral: {
+          100: "#FFEAD2",
+        },
+      },
+      fontFamily: {
+        alstoria: "Alstoria",
+        creato: "Creato Display",
       },
     },
   },
   plugins: [],
-}
+};
