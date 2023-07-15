7224cc57cd6286b381ceed128ada0b38baff2a53
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-15 14:08:52 +0700

    feat: render Hero and KesatriaMuda

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Hero from './components/Hero'
+import KesatriaMuda from './components/KesatriaMuda'
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <main>
+        <Hero />
+        <KesatriaMuda />
       </main>
   )
 }
