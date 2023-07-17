b1c69cfd5a29e45523fd2605d8046c47e87b85b3
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-17 20:03:27 +0700

    feat: render section VideoSejarah

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Hero from './components/Hero'
 import KesatriaMuda from './components/KesatriaMuda'
+import VideoSejarah from './components/VideoSejarah'
 
 export default function Home() {
   return (
       <main>
         <Hero />
+        <VideoSejarah />
         <KesatriaMuda />
       </main>
   )
