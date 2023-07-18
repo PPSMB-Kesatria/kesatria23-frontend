dc060e957c97dfdfd9caabc74ba094cf406d19f2
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-18 20:34:38 +0700

    feat: add video placeholder and image

--- src/app/components/ui/videoplaceholder.jsx
+++ src/app/components/ui/videoplaceholder.jsx
@@ -1,10 +1,21 @@
+import Image from "next/image"
+
 export const PlaceholderVideoSejarah1 = () => {
-    return(
-        <>
-          <div className="w-full">
-            <div className="bg-red-100 min-h-[250px] rounded-3xl">
-            </div>
+  return(
+    <>
+      <div className="relative flex justify-center items-center rounded-2xl min-w-[400px] aspect-video bg-red-100">
+        <div className="absolute z-10">
+          <div className="h-52 aspect-video overflow-hidden rounded-xl">
+            <iframe
+            src="https://www.youtube.com/embed/cAfRzKW-Tyw"
+            className="w-full h-full"
+            allowfullscreen />
           </div>
-        </>
-    )
+        </div>
+        <div className="absolute z-[11] w-96 flex justify-end -mt-48 ml-64">
+          <Image src="/bird1.svg" width={250} height={100}/>
+        </div>
+      </div>
+    </>
+  )
 }
