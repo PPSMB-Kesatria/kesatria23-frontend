49d56e8b9e84b1c4053a492f52066c02dd683f25
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-19 13:13:46 +0700

    feat: preparation for KontribusiMasaDepan component

---
+++ src/app/components/KontribusiMasaDepan.jsx
@@ -0,0 +1,10 @@
+export default function KontribusiMasaDepan() {
+  return(
+    <section className="bg-pattern-cream min-h-screen relative flex flex-col justify-center items-center text-green-400">
+      <img src="/TransitionGreenEnd.png" alt="" className="w-full absolute top-0 translate-y-[-50%] drop-shadow-[0_10px_10px_rgba(0,0,0,.5)]" />
+      <h1 className="font-alstoria text-[45px] 2xl:text-[60px] text-center mb-7 mt-5 md:mt-10">
+          Kontribusi Di Masa Depan
+        </h1>
+    </section>
+  )
+}
