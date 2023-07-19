6545ed41c400a2cac4f3d3f93bb291b780d85301
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-19 13:25:42 +0700

    feat: add metadata

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,22 @@
-import Hero from './components/Hero'
-import KesatriaMuda from './components/KesatriaMuda'
-import KontribusiMasaDepan from './components/KontribusiMasaDepan'
-import VideoSejarah from './components/VideoSejarah'
+import Head from "next/head";
+import Hero from "./components/Hero";
+import KesatriaMuda from "./components/KesatriaMuda";
+import KontribusiMasaDepan from "./components/KontribusiMasaDepan";
+import VideoSejarah from "./components/VideoSejarah";
+
+export const metadata = {
+  title: "Beranda · PPSMB Kesatria 2023",
+  description:
+    "PPSMB Kesatria merupakan masa orientasi kepada Mahasiswa Baru Teknik UGM 2023 yang bertujuan untuk memperkenalkan lingkungan Teknik UGM kepada para Mahasiswa Baru",
+};
 
 export default function Home() {
   return (
-      <main className="overflow-x-hidden">
-        <Hero />
-        <VideoSejarah />
-        <KesatriaMuda />
-        <KontribusiMasaDepan />
-      </main>
-  )
+    <main className="overflow-x-hidden">
+      <Hero />
+      <VideoSejarah />
+      <KesatriaMuda />
+      <KontribusiMasaDepan />
+    </main>
+  );
 }
