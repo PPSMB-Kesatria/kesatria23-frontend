f79f1e2a632c0a2f6ddf163dfe643d6dd1fb6c4b
inamnurul <aam.inam7310@gmail.com>
2023-07-20 19:25:35 +0700

    add: dokumentasi assets

--- src/app/dokumentasi/page.jsx
+++ src/app/dokumentasi/page.jsx
@@ -1,66 +1,77 @@
 "use client";
-import Link from "next/link";
+
+import Top from "../../../public/top_dokumentasi.svg"
+import Bot from "../../../public/bot_dokumentasi.svg"
+import Silhouette from "../../../public/silhouette_tugu_teknik_hero.svg"
+
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 export default function Dokumentasi() {
   const [swiper, setSwiper] = useState(null);
-
-  // Define breakpoints for different screen sizes
+  
+  
   const breakpoints = {
-    320: { slidesPerView: 1 }, // Mobile view with 1 slide per view
-    640: { slidesPerView: 2 }, // Larger screens with 2 slides per view
-    1024: { slidesPerView: 3 }, // Even larger screens with 4 slides per view
+    320: { slidesPerView: 1 },
+    640: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
   };
-
+  
   return (
     <section className="min-h-screen text-neutral-100 py-10 md:py-20 relative bg-pattern-green bg-contain bg-repeat flex gap-[35px] justify-center items-center flex-col">
-      <div>
+      <Image src={Silhouette} width={1920} height={220}  className="absolute bottom-0 right-0 min-w-[600px]"/>
+      <Image src={Top} className="absolute top-0 left-0 sm:w-[70%] md:w-[60%] lg:w-[50%]" width={700}/>
+      <Image src={Bot} className="absolute bottom-0 right-0 sm:w-[70%] md:w-[60%] lg:w-[50%]" width={700}/>
+
+
+
+      <div className="z-[100]">
         <h1 className="font-alstoria text-[45px] 2xl:text-[60px] text-center mb-7 mt-5 md:mt-10">
           Dokumentasi
         </h1>
       </div>
 
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        className="w-full !overflow-y-visible !px-[10px]"
-        grabCursor={true}
-        onSwiper={setSwiper}
-        breakpoints={breakpoints} // Apply breakpoints
-      >
-        <SwiperSlide>
-          <Imagecard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Imagecard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Imagecard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Imagecard />
-        </SwiperSlide>
-        <div
-          className="z-[1] hidden cursor-pointer unselectable lg:block absolute left-[10%] text-[80px] top-[50%] translate-y-[-50%]"
-          onClick={() => {
-            if (swiper.activeIndex === 0) return;
-            swiper.slidePrev();
-          }}
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          className="w-[290px] sm:w-[576px] lg:w-[920px] 2xl:w-[1020px] !overflow-y-visible !px-[10px] bg-green-100 rounded-lg"
+          grabCursor={true}
+          onSwiper={setSwiper}
+          breakpoints={breakpoints}
         >
-          <IoIosArrowBack className="text-green-400" />
-        </div>
-        <div
-          className="z-[1] hidden cursor-pointer unselectable lg:block absolute right-[10%] text-[80px] top-[50%] translate-y-[-50%]"
-          onClick={() => {
-            if (swiper.activeIndex === 3) return;
-            swiper.slideNext();
-          }}
-        >
-          <IoIosArrowForward className="text-green-400" />
-        </div>
-      </Swiper>
+          <SwiperSlide>
+            <Imagecard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Imagecard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Imagecard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Imagecard />
+          </SwiperSlide>
+          <div
+            className="z-[1] hidden cursor-pointer unselectable lg:block absolute left-[10%] text-[80px] top-[50%] translate-y-[-50%] "
+            onClick={() => {
+              if (swiper.activeIndex === 0) return;
+              swiper.slidePrev();
+            }}
+          >
+            <IoIosArrowBack className="text-green-400 h-[200%]" />
+          </div>
+          <div
+            className="z-[1] hidden cursor-pointer unselectable lg:block absolute right-[10%] text-[80px] top-[50%] translate-y-[-50%]"
+            onClick={() => {
+              if (swiper.activeIndex === 3) return;
+              swiper.slideNext();
+            }}
+          >
+            <IoIosArrowForward className="text-green-400" />
+          </div>
+        </Swiper>
     </section>
   );
 }
@@ -68,10 +79,10 @@ export default function Dokumentasi() {
 function Imagecard() {
   const placeholders = Array.from({ length: 2 });
   return (
-    <div className="flex flex-col lg:flex-row ml-[20px] !mx-auto p-5 sm:rounded-[40px] gap-10 w-fit items-center sm:px-10 sm:py-10 2xl:py-12 2xl:px-20">
-      <div className="container mx-auto w-fit flex flex-wrap justify-center">
+    <div className="flex flex-col lg:flex-row ml-[20px] sm:rounded-[40px] sw-fit items-center  sm:py-10 2xl:py-12 !mx-auto">
+      <div className=" mx-auto w-64 flex flex-wrap justify-center py-2">
         {placeholders.map((_, index) => (
-          <div key={index} className=" w-64 h-36 mb-4 animate-pulse m-2">
+          <div key={index} className=" w-64 h-36 my-2 animate-pulse m-2">
             <div className="bg-gray-300 w-full h-full"></div>
           </div>
         ))}
