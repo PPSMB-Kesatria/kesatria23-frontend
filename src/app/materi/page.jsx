e88ccea2a6b8159f262007262a7b5738ec04232d
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-22 02:09:20 +0700

    fix: render contents into page

--- src/app/materi/page.jsx
+++ src/app/materi/page.jsx
@@ -1,32 +1,46 @@
-import Image from "next/image"
+"use client"
+import { useEffect, useState } from 'react'
+import { CustomButton } from './components/CustomButton'
+import { PersonalBranding } from "./contents/PersonalBranding"
+import { FasilitasFakultas } from "./contents/FasilitasFakultas"
 
 export default function Materi() {
+  
+  const MATERI_NAVIGATION = [
+    {
+      title: 'Personal Branding',
+      content: <PersonalBranding />,
+      isClicked: true,
+    },
+    {
+      title: 'Fasilitas Fakultas',
+      content: <FasilitasFakultas />,
+      isClicked: false,
+    },
+  ]
+
+  const [currentPage, setCurrentPage] = useState(MATERI_NAVIGATION[0])
+
+  const handlePageChange = (idx) => {
+    setCurrentPage(MATERI_NAVIGATION[idx])
+  }
+
+
   return(
     <main className="bg-pattern-green min-h-screen bg-repeat bg-contain gap-4 flex flex-col items-center py-36">
       <h1 className="font-alstoria text-6xl text-neutral-100">Materi</h1>
       <div className="flex flex-row gap-10 justify-center w-4/5">
-        <aside className="space-y-4 min-w-fit flex flex-col items-center border w-1/5">
-          <button className="px-5 py-2.5 rounded-full border-[4px] border-neutral-100 text-neutral-100">Personal Branding</button>
-          <button className="px-5 py-2.5 rounded-full border-[4px] border-neutral-100 text-neutral-100">Personal Branding</button>
-          <button className="px-5 py-2.5 rounded-full border-[4px] border-neutral-100 text-neutral-100">Personal Branding</button>
-          <button className="px-5 py-2.5 rounded-full border-[4px] border-neutral-100 text-neutral-100">Personal Branding</button>
-
+        <aside className="space-y-8 min-w-fit flex flex-col items-center border w-1/5">
+          <button onClick={()=>{handlePageChange(0)}}>
+            <CustomButton isClicked={MATERI_NAVIGATION[0].isClicked}>Personal Branding</CustomButton>
+          </button>
+          <button onClick={()=>{handlePageChange(1)}}>
+            <CustomButton isClicked={MATERI_NAVIGATION[1].isClicked}>Fasilitas Fakultas</CustomButton>
+          </button>
         </aside>
         <content className="min-h-screen border max-w-6xl border-red-500 space-y-4 w-4/5">
-          <h1 className="text-neutral-100 font-creato font-bold text-4xl">Personal Branding</h1>
-          <p className="text-neutral-100 text-justify font-creato">
-            Personal Branding adalah suatu proses pembentukan persepsi
-            masyarakat terhadap aspek yang dimiliki individu. Dalam materi
-            Personal Branding ini, Kesatria Muda akan mempelajari tentang
-            bagaimana cara melakukan Personal Branding yang baik, seperti membuat
-            CV dan resume yang benar. Selain itu, Kesatria juga akan dikenalkan 
-            dengan Applicant Tracking System (ATS) dan aplikasi LinkedIn.</p>
-          <div className="flex flex-col justify-center">
-            <Image src="/ats-cv-sample.jpg"
-              alt="Contoh CV ATS Friendly"
-              title="Contoh CV ATS Friendly"
-              height={200} width={1920} />
-            <p className="text-neutral-100 text-center italic font-creato">Gambar: Contoh CV ATS Friendly</p>
+          <div id="content">
+            {currentPage.content}
           </div>
         </content>
       </div>
