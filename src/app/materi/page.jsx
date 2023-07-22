838853aa3f23e430c883c0abeb44edb192200437
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-22 12:16:46 +0700

    misc: remove border

--- src/app/materi/page.jsx
+++ src/app/materi/page.jsx
@@ -23,7 +23,7 @@ export default function Materi() {
 
   const ButtonClicked = ({title}) => {
     return(
-      <div className="border flex cursor-pointer">
+      <div className="flex cursor-pointer">
         <div className={"relative z-[10] px-10 py-2.5 rounded-2xl font-creato font-bold "+
         "shadow-md shadow-black/30 text-neutral-100 outline outline-[6px] outline-neutral-100"}>
           {title}
@@ -37,7 +37,7 @@ export default function Materi() {
 
   const ButtonNotClicked = ({onClick, title}) => {
     return(
-      <div onClick={onClick} className="border flex cursor-pointer">
+      <div onClick={onClick} className="flex cursor-pointer">
         <div className={"relative z-[10] px-10 py-2.5 rounded-2xl font-creato font-bold shadow-md shadow-black/30 text-green-400 bg-neutral-100"}>
           {title}
         </div>
@@ -52,7 +52,7 @@ export default function Materi() {
     <main className="bg-pattern-green min-h-screen bg-repeat bg-contain gap-4 flex flex-col items-center py-36">
       <h1 className="font-alstoria text-6xl text-neutral-100">Materi</h1>
       <div className="flex flex-row gap-10 justify-center w-4/5">
-        <aside className="space-y-8 min-w-fit flex flex-col items-center border w-1/5">
+        <aside className="space-y-8 min-w-fit flex flex-col items-center w-1/5">
           {
             activeIndex === 0 ? <ButtonClicked title={MATERI_NAVIGATION[0].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(0)}} title={MATERI_NAVIGATION[0].title}/>
           }
@@ -63,7 +63,7 @@ export default function Materi() {
             activeIndex === 2 ? <ButtonClicked title={MATERI_NAVIGATION[2].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(2)}} title={MATERI_NAVIGATION[2].title}/>
           }
         </aside>
-        <content className="min-h-screen border max-w-6xl border-red-500 space-y-4 w-4/5">
+        <content className="min-h-screen max-w-6xl space-y-4 w-4/5">
           {MATERI_NAVIGATION[activeIndex].content}
         </content>
       </div>
