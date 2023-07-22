4e28f8f476ad39dd78ec6a966f99bfc8628bc4f1
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-22 10:18:13 +0700

    feat: add aos on Hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,11 +1,20 @@
+"use client";
 import Image from "next/image";
 import Nala from "../../../public/nala_hero.png";
 import Topbar from "../../../public/topbar_hero.svg";
 import Botbar_1 from "../../../public/bot_hero_1.svg";
 import Botbar_2 from "../../../public/bot_hero_2.svg";
 import Silhouette from "../../../public/silhouette_tugu_teknik_hero.svg";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Hero() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+    });
+  });
   return (
     <section className="min-h-screen bg-pattern-green bg-repeat z-[0] flex">
       <Image
@@ -13,6 +22,7 @@ export default function Hero() {
         className=" -top-[20px] absolute w-[100%] min-w-[1100px] right-0"
         alt=""
         width={1920}
+        data-aos="fade-down"
       />
       <div className="flex flex-col lg:flex-row justify-center items-center content-center relative z-[3] w-full gap-8 xl:gap-10 md:px-5">
         <Image
@@ -20,8 +30,9 @@ export default function Hero() {
           width={1400}
           alt="Nala"
           className="w-[85%] flex-shrink-[80%] sm:w-[50%] sm:p-0 md:w-[75%] lg:w-[50%] h-fit min-w-[200px] max-w-[480px] 2xl:max-w-[600px] relative z-[2]"
+          data-aos="fade-up"
         />
-        <div className="flex w-fit justify-center //bg-red-500/60 flex-col text-white z-[100]">
+        <div data-aos="fade-up" className="flex w-fit justify-center //bg-red-500/60 flex-col text-white z-[100]">
           <div className="font-alstoria lg:text-[54px] mb-[10px] md:text-[49px] sm:text-[45px] text-[40px] text-neutral-100 drop-shadow-lg text-center lg:text-left leading-[100%]">
             Selamat Datang{" "}
           </div>
@@ -34,7 +45,7 @@ export default function Hero() {
           </div>
         </div>
       </div>
-      <div className="self-end flex absolute left-0 right-0 w-[100%]">
+      <div data-aos="fade-up" data-aos-offset="-200" data-aos-delay="300" className="self-end flex absolute left-0 right-0 w-[100%]">
         <Image
           src={Silhouette}
           width={1920}
