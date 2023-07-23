29e3ca18bb3d3d0a39e844d6e99ae871c1db0cfa
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-23 23:11:12 +0700

    feat: Add intro to section

--- src/app/components/VideoSejarah.jsx
+++ src/app/components/VideoSejarah.jsx
@@ -10,8 +10,20 @@ export default function VideoSejarah() {
           className="bottom-0 left-0 min-w-[700px] z-[2]"
         />
       </div>
-      <div className="min-h-screen flex flex-col bg-pattern-cream-2 bg-contain bg-repeat justify-center items-center pt-12 pb-36 lg:pt-36 lg:pb-48 gap-20 lg:gap-15">
-        
+      <div className="min-h-screen flex flex-col bg-pattern-cream-2 bg-contain bg-repeat justify-center items-center pt-[5.5rem] pb-36 md:pt-[7rem] lg:pt-36 lg:pb-48 gap-20 lg:gap-15">
+        <div data-aos="fade-up" about="Intro to Section" className="text-center w-4/5 md:w-2/3 space-y-5 lg:pt-0">
+          <div className="font-alstoria text-green-400">
+            <h1 className="text-[40px] leading-[1] md:text-6xl">Halo Kesatria Muda<span className="font-serif font-bold">!</span></h1>
+            <h2 className="text-[30px] leading-[1] md:text-3xl">Selamat datang di Chapter <span className="font-serif font-bold">1:</span> Aku yang dulu</h2>
+          </div>
+          <p className="font-creato font-medium text-[18px] sm:text-[20px] 2xl:text-[25px] text-green-400 text-center">
+            Memasuki masa perkuliahan, personal branding merupakan hal yang wajib diketahui. 
+            Melalui chapter ini, Kesatria Muda akan mempelajari cara untuk membangun personal 
+            branding yang kuat melalui CV dan Analisis SWOT.  Selain modul utama mengenai personal 
+            branding, disini juga terdapat 2 video menarik yaitu “Sejarah Teknik” dan 
+            “Sejarah Kesatria” yang wajib ditonton sebelum kita bertemu secara offline di PPSMB 
+            Kesatria nanti.</p>
+        </div>
         {/*Sejarah Teknik*/}
         <div about="Sejarah Teknik" className="flex flex-col lg:flex-row w-4/5 gap-14 lg:gap-28 items-center justify-center">
           <div data-aos="fade-up">
