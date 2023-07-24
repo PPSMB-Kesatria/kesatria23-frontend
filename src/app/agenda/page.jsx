5c3b46c75771f3a4da309948f47a75f9fb72e2de
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-25 05:53:19 +0700

    feat: update button for smoother transition

--- src/app/agenda/page.jsx
+++ src/app/agenda/page.jsx
@@ -1,46 +1,66 @@
-"use client"
+"use client";
 import { useState } from "react";
 import { Day1 } from "./rundown/day1";
 import { Day2 } from "./rundown/day2";
 
-
 export default function Agenda() {
-  const [activeIndex, setActiveIndex] = useState(0)
-  
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const RUNDOWN_NAVIGATION = [
     {
       title: "Hari Pertama",
-      content: <Day1 />
+      content: <Day1 />,
     },
     {
       title: "Hari Kedua",
-      content: <Day2 />
+      content: <Day2 />,
     },
-  ]
-  const ButtonClicked = ({title}) =>{
-    return(
-      <button className="cursor-pointer my-2 py-2 w-[85px] xs:w-[20%] lg:w-[204.8px] bg-red-200 text-neutral-100 rounded-xl drop-shadow-xl font-creato font-medium">{title}</button>
-    )
-  }
-  const ButtonNotClicked = ({onClick,title})=>{
-    return(
-      <button onClick={onClick} className="cursor-pointer my-2 p-2 w-[85px] xs:w-[20%] lg:w-[204.8px] bg-neutral-100 text-red-200 rounded-xl drop-shadow-xl font-creato">{title}</button>
-    )
-  }
-  return(
+  ];
+
+  return (
     <main className=" bg-pattern-cream bg-repeat py-32 lg:pb-40">
-      <h1 data-aos="fade-up" className=" font-alstoria text-center text-[48px] text-green-400 mb-7 ">Agenda</h1>
-      <div data-aos="fade-up" className="flex flex-row place-content-center gap-[1%] mb-12">
-          {
-            activeIndex === 0 ? <ButtonClicked title={RUNDOWN_NAVIGATION[0].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(0)}} title={RUNDOWN_NAVIGATION[0].title}/>
-          }
-          {
-            activeIndex === 1 ? <ButtonClicked title={RUNDOWN_NAVIGATION[1].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(1)}} title={RUNDOWN_NAVIGATION[1].title}/>
-          }
+      <h1
+        data-aos="fade-up"
+        className=" font-alstoria text-center text-[48px] text-green-400 mb-7 "
+      >
+        Agenda
+      </h1>
+      <div
+        data-aos="fade-up"
+        className="flex flex-row place-content-center gap-[1%] mb-12"
+      >
+        <Button
+          title="Hari Pertama"
+          ownIndex={0}
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+        />
+        <Button
+          title="Hari Kedua"
+          ownIndex={1}
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+        />
       </div>
       {RUNDOWN_NAVIGATION[activeIndex].content}
-
-      
     </main>
   );
 }
+
+const Button = ({ title, ownIndex, activeIndex, setActiveIndex }) => {
+  return (
+    <button
+      className={
+        "cursor-pointer my-2 py-2 w-[85px] transition-all xs:w-[20%] lg:w-[204.8px] rounded-xl drop-shadow-xl font-creato font-medium " +
+        (ownIndex == activeIndex
+          ? "bg-red-200 text-neutral-100"
+          : "bg-neutral-100 text-red-200 hover:shadow-[0_0_10px_#F34B4C]")
+      }
+      onClick={() => {
+        setActiveIndex(ownIndex);
+      }}
+    >
+      {title}
+    </button>
+  );
+};
