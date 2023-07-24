4392cb214dc4476cb5d7af223ac0a91ff81ec21c
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-24 10:46:21 +0700

    feat: Add bottom ornaments

--- src/app/materi/page.jsx
+++ src/app/materi/page.jsx
@@ -23,7 +23,7 @@ export default function Materi() {
 
   const ButtonClicked = ({title}) => {
     return(
-      <div className="flex cursor-pointer">
+      <div className="flex cursor-pointer select-none">
         <div className={"relative z-[10] px-10 py-2.5 rounded-2xl font-creato font-bold "+
         "shadow-md shadow-black/30 text-neutral-100 outline outline-[6px] outline-neutral-100"}>
           {title}
@@ -37,7 +37,7 @@ export default function Materi() {
 
   const ButtonNotClicked = ({onClick, title}) => {
     return(
-      <div onClick={onClick} className="flex cursor-pointer">
+      <div onClick={onClick} className="flex cursor-pointer select-none">
         <div className={"relative z-[10] px-10 py-2.5 rounded-2xl font-creato font-bold shadow-md shadow-black/30 text-green-400 bg-neutral-100"}>
           {title}
         </div>
@@ -49,24 +49,28 @@ export default function Materi() {
   }
 
   return(
-    <main className="bg-pattern-green min-h-screen bg-repeat bg-contain gap-4 flex flex-col items-center py-36">
-      <h1 className="font-alstoria text-6xl text-neutral-100">Materi</h1>
-      <div className="flex flex-row gap-10 justify-center w-4/5">
-        <aside className="space-y-8 min-w-fit flex flex-col items-center w-1/5">
-          {
-            activeIndex === 0 ? <ButtonClicked title={MATERI_NAVIGATION[0].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(0)}} title={MATERI_NAVIGATION[0].title}/>
-          }
-          {
-            activeIndex === 1 ? <ButtonClicked title={MATERI_NAVIGATION[1].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(1)}} title={MATERI_NAVIGATION[1].title}/>
-          }
-          {
-            activeIndex === 2 ? <ButtonClicked title={MATERI_NAVIGATION[2].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(2)}} title={MATERI_NAVIGATION[2].title}/>
-          }
-        </aside>
-        <content className="min-h-screen max-w-6xl space-y-4 w-4/5">
-          {MATERI_NAVIGATION[activeIndex].content}
-        </content>
-      </div>
-    </main>
+    <div className="relative">
+      <main className="bg-pattern-green min-h-screen bg-repeat bg-contain gap-4 flex flex-col items-center pt-36 pb-36 md:pb-28 lg:pb-64">
+        <h1 className="font-alstoria text-6xl text-neutral-100">Materi</h1>
+        <div className="flex flex-row gap-10 justify-center w-4/5">
+          <aside className="hidden md:flex md:flex-col items-center w-1/5 space-y-8 min-w-fit ">
+            {
+              activeIndex === 0 ? <ButtonClicked title={MATERI_NAVIGATION[0].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(0)}} title={MATERI_NAVIGATION[0].title}/>
+            }
+            {
+              activeIndex === 1 ? <ButtonClicked title={MATERI_NAVIGATION[1].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(1)}} title={MATERI_NAVIGATION[1].title}/>
+            }
+            {
+              activeIndex === 2 ? <ButtonClicked title={MATERI_NAVIGATION[2].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(2)}} title={MATERI_NAVIGATION[2].title}/>
+            }
+          </aside>
+          <content className="min-h-screen max-w-6xl space-y-4 md:w-4/5">
+            {MATERI_NAVIGATION[activeIndex].content}
+          </content>
+        </div>
+      </main>
+      <img src="/materi_bottom_ornament.png" className="-mt-[5.5rem] sm:mt-0 z-[2] absolute w-screen min-w-[560px] sm:min-w-[1000px] overflow-hidden left-0 md:right-0 sm:bottom-0" />
+      <img src="/tugu_teknik_materi.svg" className="hidden md:block w-screen absolute md:h-[36rem] lg:h-[50rem] z-[1] bottom-0 md:-ml-72 lg:-ml-96"/>
+    </div>
   )
 }
