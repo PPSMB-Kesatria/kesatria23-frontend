4c9b7f6cd107b6d62fb37fc628e521ee2d7ad304
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-24 20:56:52 +0700

    fix: change button on materi for better transition

--- src/app/materi/page.jsx
+++ src/app/materi/page.jsx
@@ -1,76 +1,66 @@
-"use client"
-import { useState } from 'react'
-import { PersonalBranding } from "./contents/PersonalBranding"
-import { FasilitasFakultas } from "./contents/FasilitasFakultas"
-import { DummyContent } from './contents/DummyContent'
+"use client";
+import { useState } from "react";
+import { PersonalBranding } from "./contents/PersonalBranding";
+import { FasilitasFakultas } from "./contents/FasilitasFakultas";
+import { DummyContent } from "./contents/DummyContent";
 
 export default function Materi() {
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
   const MATERI_NAVIGATION = [
     {
       title: "Personal Branding",
-      content: <PersonalBranding />
+      content: <PersonalBranding />,
     },
     {
       title: "Fasilitas Fakultas",
-      content: <FasilitasFakultas />
+      content: <FasilitasFakultas />,
     },
     {
       title: "Dummy Content",
-      content: <DummyContent />
+      content: <DummyContent />,
     },
-  ]
+  ];
 
-  const ButtonClicked = ({title}) => {
-    return(
-      <div className="flex cursor-pointer select-none">
-        <div className={"relative z-[10] px-10 py-2.5 rounded-2xl font-creato font-bold "+
-        "shadow-md shadow-black/30 text-neutral-100 outline outline-[6px] outline-neutral-100"}>
-          {title}
-        </div>
-        <div className={"absolute z-[9] mt-[5px] ml-[3px] px-10 py-2.5 rounded-2xl font-creato text-transparent font-bold shadow-md shadow-black/30 outline outline-[6px] outline-red-100"}>
-          {title}
-        </div>
+  const Button = ({ title, ownIndex }) => {
+    return (
+      <div
+        className={
+          "w-full relative flex justify-center items-center py-3.5 rounded-2xl font-creato font-bold shadow-md shadow-black/30 cursor-pointer !transition-all duration-300 after:box-border text-green-400 after:w-full after:absolute after:bg-red-100 after:top-[5px] after:left-[3px] after:h-full after:rounded-2xl z-[2] preserve-3d after-z " +
+          (ownIndex == activeIndex ? " shadow-none after:bg-transparent text-neutral-100 after:!border-red-100 after:border-[6px] outline-neutral-100 outline-[6px] before:absolute before:w-full before:h-full before:border-neutral-100 before:border-[6px] before:rounded-2xl" : "bg-neutral-100")
+        }
+        onClick={() => {
+          setActiveIndex(ownIndex);
+        }}
+      >
+        <h1 className="px-10">{title}</h1>
       </div>
-    )
-  }
+    );
+  };
 
-  const ButtonNotClicked = ({onClick, title}) => {
-    return(
-      <div onClick={onClick} className="flex cursor-pointer select-none">
-        <div className={"relative z-[10] px-10 py-2.5 rounded-2xl font-creato font-bold shadow-md shadow-black/30 text-green-400 bg-neutral-100"}>
-          {title}
-        </div>
-        <div className={"absolute z-[9] mt-[5px] ml-[3px] px-10 py-2.5 rounded-2xl font-creato text-transparent font-bold shadow-md shadow-black/30 bg-red-100"}>
-          {title}
-        </div>
-      </div>
-    )
-  }
-
-  return(
+  return (
     <div className="relative overflow-hidden">
       <main className="bg-pattern-green min-h-screen bg-repeat bg-contain gap-4 flex flex-col items-center pt-36 pb-36 md:pb-28 lg:pb-64">
         <h1 className="font-alstoria text-6xl text-neutral-100">Materi</h1>
         <div className="flex flex-row gap-10 justify-center w-4/5">
           <aside className="hidden md:flex md:flex-col items-center w-1/5 space-y-8 min-w-fit ">
-            {
-              activeIndex === 0 ? <ButtonClicked title={MATERI_NAVIGATION[0].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(0)}} title={MATERI_NAVIGATION[0].title}/>
-            }
-            {
-              activeIndex === 1 ? <ButtonClicked title={MATERI_NAVIGATION[1].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(1)}} title={MATERI_NAVIGATION[1].title}/>
-            }
-            {
-              activeIndex === 2 ? <ButtonClicked title={MATERI_NAVIGATION[2].title} /> : <ButtonNotClicked onClick={()=>{setActiveIndex(2)}} title={MATERI_NAVIGATION[2].title}/>
-            }
+            <Button title="Personal Branding" ownIndex={0} />
+            <Button title="Fasilitas Fakultas" ownIndex={1} />
+            <Button title="Dummy Content" ownIndex={2} />
+            {/* <Button title="Test" ownIndex={3} /> */}
           </aside>
           <content className="min-h-screen max-w-6xl space-y-4 md:w-4/5">
             {MATERI_NAVIGATION[activeIndex].content}
           </content>
         </div>
       </main>
-      <img src="/materi_bottom_ornament.png" className="-mt-[5.5rem] sm:mt-0 z-[2] absolute w-screen min-w-[560px] sm:min-w-[1000px] overflow-hidden left-0 md:right-0 sm:bottom-0" />
-      <img src="/tugu_teknik_materi.svg" className="hidden md:block w-screen absolute md:h-[36rem] lg:h-[50rem] z-[1] bottom-0 md:-ml-72 lg:-ml-96"/>
+      <img
+        src="/materi_bottom_ornament.png"
+        className="-mt-[5.5rem] sm:mt-0 z-[2] absolute w-screen min-w-[560px] sm:min-w-[1000px] overflow-hidden left-0 md:right-0 sm:bottom-0"
+      />
+      <img
+        src="/tugu_teknik_materi.svg"
+        className="hidden md:block w-screen absolute md:h-[36rem] lg:h-[50rem] z-[1] bottom-0 md:-ml-72 lg:-ml-96"
+      />
     </div>
-  )
+  );
 }
