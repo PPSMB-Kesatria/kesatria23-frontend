a03acd8d9a1d7a0f5605b6c5f14755cf79c598de
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-25 04:45:18 +0700

    feat: update title to Short Movie

--- src/app/components/KesatriaMuda.jsx
+++ src/app/components/KesatriaMuda.jsx
@@ -186,7 +186,7 @@ export default function KesatriaMuda() {
           className="bg-neutral-100 max-w-[940px] mt-5 lg:mt-0 p-5 py-10 sm:p-10 lg:pl-20 text-green-400 w-[90%] lg:w-1/2 h-fit rounded-l-[40px] lg:rounded-l-none 2xl:rounded-l-[40px] rounded-r-[40px]"
         >
           <h1 className="font-alstoria text-[45px] 2xl:text-[60px] leading-[100%] text-center lg:text-left">
-            Bridging Movie
+            Short Movie
           </h1>
           <p className="text-[18px] md:text-[20px] 2xl:text-[25px] font-creato text-justify">
             Mengenal lingkungan teknik tentunya juga termasuk mengenal
