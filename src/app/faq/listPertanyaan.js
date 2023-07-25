f2b94ae5008b11706341513f3f2b3c8cbb73bd1e
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-25 11:24:01 +0700

    separate listPertanyaan

---
+++ src/app/faq/listPertanyaan.js
@@ -0,0 +1,42 @@
+const listPertanyaanUmum = [
+  {
+    question: "Pertanyaan Umum 1",
+    answer: "Jawaban Pertanyaan Umum 1",
+  },
+  {
+    question: "Pertanyaan Umum 2",
+    answer: "Jawaban Pertanyaan Umum 2",
+  },
+  {
+    question: "Pertanyaan Umum 3",
+    answer: "Jawaban Pertanyaan Umum 3",
+  },
+  {
+    question: "Pertanyaan Umum 4",
+    answer: "Jawaban Pertanyaan Umum 4",
+  },
+]
+
+const listPertanyaanMateri = [
+  {
+    question: "Pertanyaan Materi 1",
+    answer: "Jawaban Pertanyaan Materi 1",
+  },
+  {
+    question: "Pertanyaan Materi 2",
+    answer: "Jawaban Pertanyaan Materi 2",
+  },
+  {
+    question: "Pertanyaan Materi 3",
+    answer: "Jawaban Pertanyaan Materi 3",
+  },
+  {
+    question: "Pertanyaan Materi 4",
+    answer: "Jawaban Pertanyaan Materi 4",
+  },
+]
+
+module.exports = {
+  listPertanyaanUmum,
+  listPertanyaanMateri
+}
