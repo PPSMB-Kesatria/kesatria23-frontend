defe839801d9e6a503ecc33b5ddf57ef0f3796e6
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-26 01:12:54 +0700

    feat: display pdf module using iframe

--- src/app/materi/contents/PersonalBranding.jsx
+++ src/app/materi/contents/PersonalBranding.jsx
@@ -1,22 +1,11 @@
-import Image from "next/image"
-
 export const PersonalBranding = () => {
+
     return(
         <div className="space-y-5">
-            <h1 className="text-neutral-100 font-creato font-bold text-4xl">Personal Branding</h1>
-                <p className="text-neutral-100 text-justify font-creato">
-                Personal Branding adalah suatu proses pembentukan persepsi
-                masyarakat terhadap aspek yang dimiliki individu. Dalam materi
-                Personal Branding ini, Kesatria Muda akan mempelajari tentang
-                bagaimana cara melakukan Personal Branding yang baik, seperti membuat
-                CV dan resume yang benar. Selain itu, Kesatria juga akan dikenalkan 
-                dengan Applicant Tracking System (ATS) dan aplikasi LinkedIn.</p>
-            <div className="flex flex-col items-center">
-                <Image src="/ats-cv-sample.jpg"
-                alt="Contoh CV ATS Friendly"
-                title="Contoh CV ATS Friendly"
-                height={200} width={500} />
-                <p className="text-neutral-100 text-center italic font-creato">Gambar: Contoh CV ATS Friendly</p>
+            <h1 className="text-neutral-100 font-creato font-bold text-4xl">Personal Branding dan SWOT</h1>
+            <div className="flex justify-center">
+                <iframe src="https://drive.google.com/file/d/1wCdC7cDT-8jkm0b_TQkucXoUXxTcArfr/preview"
+                allow="autoplay" className="w-3/4 aspect-[.75]"/>
             </div>
         </div>
     )
